dfd3aaa58e060c5f7450e6d6ac9198c60ba08aeb
Ayush Singh <ayushsingh123102@gmail.com>
2026-07-09 01:34:52 +0530

    Debugged login error

--- SpringDemo/src/main/java/com/example/SpringDemo/dto/Login.java
+++ SpringDemo/src/main/java/com/example/SpringDemo/dto/Login.java
@@ -3,6 +3,7 @@ package com.example.SpringDemo.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -11,6 +12,6 @@ public class Login {
     @Min(value=1000000000,message="Mobile Number must be 10 digits")
     private Long number;
     @NotBlank(message="Password not found")
-    @Min(value=5,message="Password must be 5 characters long")
+    @Size(min=5,message="Password must be 5 characters long")
     private String password;
 }
