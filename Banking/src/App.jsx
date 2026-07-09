3e2103a6a39b9bf542f501c63133132b86c82d8a
Ayush Singh <ayushsingh123102@gmail.com>
2026-07-09 10:14:10 +0530

    Removed Unresponsive buttons

--- Banking/src/App.jsx
+++ Banking/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './Components/Login'
-import {BrowserRouter,Routes,Route} from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import Signup from './Components/Signup'
 import Dashboard from './Components/Dashboard'
 import BankAccount from './Components/BankAccount'
@@ -19,21 +19,21 @@ import Statistics from './Components/Statistics'
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login/>}></Route>
-      <Route path="/signup" element={<Signup/>}></Route>
-      <Route path="/dashboard" element={<Dashboard/>}></Route>
-      <Route path="/bank" element={<BankAccount/>}></Route>
-      <Route path="/bill" element={<Bill/>}></Route>
-      <Route path="/payment" element={<NumberTransaction/>}></Route>
-      <Route path="/balance" element={<BalanceCheck/>}></Route>
-      <Route path="/history" element={<History/>}></Route>
-      <Route path="/qr" element={<QRGenerator/>}></Route>
-      <Route path="/admin-portal" element={<AdminPanel/>}></Route>
-      <Route path="/merchant" element={<Merchant/>}></Route>
-      <Route path="/add/:number" element={<AddAccount/>}></Route>
-      <Route path="/statistics" element={<Statistics/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/bank" element={<BankAccount />}></Route>
+        <Route path="/bill" element={<Bill />}></Route>
+        <Route path="/payment" element={<NumberTransaction />}></Route>
+        <Route path="/balance" element={<BalanceCheck />}></Route>
+        <Route path="/history" element={<History />}></Route>
+        <Route path="/qr" element={<QRGenerator />}></Route>
+        <Route path="/admin-portal" element={<AdminPanel />}></Route>
+        <Route path="/merchant" element={<Merchant />}></Route>
+        <Route path="/add/:number" element={<AddAccount />}></Route>
+        <Route path="/statistics" element={<Statistics />}></Route>
+      </Routes>
     </BrowserRouter>
   )
 }
